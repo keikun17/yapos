32d30aab94f543a60788841ae3ec5ecc67b13c1d
keikun17 <keikun17@gmail.com>
2013-01-01 12:56:22 +0800

    Partially working deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,48 @@
-require 'bundler/capistrano'
+# require 'bundler/capistrano'
 
 set :application, "mlandcorp-pos"
 set :repository,  "git@github.com:keikun17/mlandcorp-pos.git"
 set :scm, "git"
+set :deploy_to, "/home/deploy/apps/#{application}"
+set :deploy_via, :checkout
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :bundle_gemfile,  "Gemfile"
+# set :bundle_dir,      File.join(fetch(:shared_path), 'bundle')
+set :bundle_flags,    "--deployment --quiet"
+set :bundle_without,  [:development, :test]
+set :bundle_cmd,      "/home/deploy/.rvm/gems/ruby-1.9.3-p194/bin/bundle" # e.g. "/opt/ruby/bin/bundle"
+set :bundle_roles,    {:except => {:no_release => true}} # e.g. [:app, :batch]"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+task :production do
+  set :branch, 'master'
+  set :user, 'deploy'
+  set :ssh_options, {:forward_agent => true}
+  role :web, "106.187.50.117"
+  role :app, "106.187.50.117"
+  role :db, "106.187.50.117", :primary => true
+end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :symlink do
+  task :db, :except => {:norelease => true} do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+  end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+  task :uploads, :except => {:norelease => true} do
+    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads" 
+  end
+end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+after "deploy:finalize_update", "symlink:db"
+after "deploy:finalize_update", "symlink:uploads"
+
