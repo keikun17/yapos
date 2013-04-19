00d3951c59b51a765ac8488ebb354c063ed0db8c
keikun17 <keikun17@gmail.com>
2013-04-19 15:31:19 +0800

    Purchase service object.

---
+++ app/poro/purchase.rb
@@ -0,0 +1,32 @@
+class Purchase
+  def self.fetch_offers
+    Offer.purchased
+  end
+
+  def self.create_or_append(source)
+    case source.class.to_s
+    when "Quote"
+      fetch_or_create_from_quote(source)
+    when "Offer"
+      fetch_or_create_from_offer(source)
+    else
+      raise("not implement for #{source.class}")
+    end
+  end
+
+  private
+
+  def self.fetch_or_create_from_quote(quote)
+    quote.offers.each do |offer|
+      fetch_or_create_from_offer(offer)
+    end
+
+    quote
+  end
+
+  def self.fetch_or_create_from_offer(offer)
+    Order.find_or_create_by_reference(offer.order_reference)
+    offer
+  end
+
+end
