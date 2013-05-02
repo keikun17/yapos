f08317d9dd70a356b4375792ed8be4f66690a87d
keikun17 <keikun17@gmail.com>
2013-05-02 14:14:08 +0800

    Nil handler.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -73,16 +73,16 @@ class Offer < ActiveRecord::Base
   def update_total_prices
     quantity = self.request_quantity.blank? ? 1 : self.request_quantity
     if self.currency == Currency::LOCAL_CURRENCY
-      self.total_buying_price = self.buying_price * quantity
-      self.total_selling_price = self.selling_price * quantity
+      (self.total_buying_price = self.buying_price * quantity) if self.buying_price
+      (self.total_selling_price = self.selling_price * quantity) if self.selling_price
     elsif !self.currency.blank?
       conversion_rate = if conversion_rate.blank?
-        Currency::CURRENCY_MAPPING[self.currency] 
+        Currency::CURRENCY_MAPPING[self.currency]
       else
         self.conversion_rate
       end
-      self.total_buying_price = self.buying_price * quantity * conversion_rate
-      self.total_selling_price = self.selling_price * quantity * conversion_rate
+      (self.total_buying_price = self.buying_price * quantity * conversion_rate) if self.buying_price
+      (self.total_selling_price = self.selling_price * quantity * conversion_rate) if self.selling_price
     else
       0
     end
