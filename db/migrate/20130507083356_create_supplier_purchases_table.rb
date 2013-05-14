dccf1bf7d3ce504271f0b795c47fea28fa553720
keikun17 <keikun17@gmail.com>
2013-05-14 09:40:04 +0800

    Fix migration prob.

--- db/migrate/20130507083356_create_supplier_purchases_table.rb
+++ db/migrate/20130507083356_create_supplier_purchases_table.rb
@@ -22,7 +22,7 @@ class CreateSupplierPurchasesTable < ActiveRecord::Migration
     add_index :supplier_purchases, :reference
 
     Offer.where('order_reference is not null').all.each do |offer|
-      unless offer.supplier_order.reference.blank?
+      unless offer.supplier_order.nil? or offer.supplier_order.reference.blank?
         a = offer.order.supplier_purchases.find_or_create_by_reference(offer.supplier_order.reference)
         puts a.inspect
       else
