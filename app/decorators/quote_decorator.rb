49ff29409089cc4be6a3d9b5bfdb92a65e0c4d46
keikun17 <keikun17@gmail.com>
2013-05-15 12:41:28 +0800

    Merge common extra offer fields.

--- app/decorators/quote_decorator.rb
+++ app/decorators/quote_decorator.rb
@@ -4,4 +4,36 @@ class QuoteDecorator < Decorator
     links.uniq!
     raw links.join(',')
   end
+
+  # Maybe this belongs here instead of the model because this
+  # is leaning more toward behavior than data
+  def offer_details_mergable?(attr)
+    details = self.offers.map(&attr).uniq
+    details.count <= 1
+  end
+
+  def solo_offer_remarks
+    if @solo_offer ||= self.offers.first
+      return @solo_offer.remarks
+    end
+  end
+
+  def solo_offer_delivery
+    if @solo_offer ||= self.offers.first
+      return @solo_offer.delivery
+    end
+  end
+
+  def solo_offer_warranty
+    if @solo_offer ||= self.offers.first
+      return @solo_offer.warranty
+    end
+  end
+
+  def solo_offer_terms
+    if @solo_offer ||= self.offers.first
+      return @solo_offer.terms
+    end
+  end
+
 end
