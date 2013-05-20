0b14d3d0a4d72be1209cddc117eb3e1768e36b5a
keikun17 <keikun17@gmail.com>
2013-05-20 13:10:59 +0800

    Refactor helper.

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,13 +1,9 @@
 module QuotesHelper
   def supplier_links(quote)
-    suppliers = quote.suppliers.to_a
-    suppliers << quote.supplier
-    suppliers.compact!
-    suppliers.uniq!
-    links = suppliers.collect do |supplier|
+    suppliers = quote.suppliers.uniq.to_a
+    suppliers.collect do |supplier|
       link_to supplier.name, supplier_path(supplier) 
-    end
-    raw links.join(', ')
+    end.join(', ').html_safe
   end
 
   def quote_row_class(quote)
