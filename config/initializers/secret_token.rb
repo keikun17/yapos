8f2f28bc701162f1e0762d007faadb3444514fa8
keikun17 <keikun17@gmail.com>
2013-05-20 12:45:18 +0800

    Correct App module.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,5 +8,5 @@ if Rails.env.test? || Rails.env.development?
   Yapos::Application.config.secret_token = '1ea7d01bcdb0b77b9c6333249094c39923dd73e6775e84d462dffc9b2fbac63be7d90c5be44ee76db3736925581dc0dbbd3febbcda8f4c1a94b9eb9bf672e48a'
 else
   raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
-  Discourse::Application.config.secret_token = ENV['SECRET_TOKEN']
+  Yapos::Application.config.secret_token = ENV['SECRET_TOKEN']
 end
