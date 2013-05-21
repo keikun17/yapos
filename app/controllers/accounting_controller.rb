27950f4be75757c05aa54fdba0d206c14d972231
keikun17 <keikun17@gmail.com>
2013-05-21 13:38:36 +0800

    Order by purchase_date desc, nulls first.

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -1,6 +1,6 @@
 class AccountingController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.order('purchase_date is null desc, purchase_date desc')
     @orders = OrderDecorator.decorate_collection(@orders)
     @total_profit = Purchase.display_total_profit
   end
