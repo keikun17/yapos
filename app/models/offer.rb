5cd1ad103283f37c18ed32b3f085fb65a0bf0b79
keikun17 <keikun17@gmail.com>
2013-06-07 17:49:39 +0800

    Fix broken scopes.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -43,7 +43,7 @@ class Offer < ActiveRecord::Base
   scope :purchased, -> { where("order_reference <> '' ") }
   scope :pending_client_order, -> { where("offers.order_reference = '' or offers.order_reference is null") }
   scope :by_supplier_order_date, -> do
-    includes(:supplier_order).order("supplier_orders.ordered_at is null desc").order("supplier_orders.ordered_at desc")
+    includes(supplier_order: :supplier_purchase).order("supplier_purchases.ordered_at is null desc").order("supplier_purchases.ordered_at desc")
   end
 
   # Client Delegation
