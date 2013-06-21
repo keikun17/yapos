9796a652ad366e6746e7302fd8e5b00c4c4b64c1
Buddy Magsipoc <keikun17@gmail.com>
2013-06-21 12:13:42 +0800

    Mysql schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -17,14 +17,14 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.integer  "attachable_id"
     t.string   "attachable_type"
     t.string   "document"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "clients", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "emails"
     t.string   "contact_numbers"
     t.text     "address"
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.string   "currency"
     t.float    "currency_conversion"
     t.string   "order_reference"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "remarks"
     t.string   "terms"
     t.string   "delivery"
@@ -52,14 +52,14 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.string   "summary"
   end
 
-  add_index "offers", ["order_reference"], name: "index_offers_on_order_reference"
-  add_index "offers", ["summary"], name: "index_offers_on_summary"
+  add_index "offers", ["order_reference"], name: "index_offers_on_order_reference", using: :btree
+  add_index "offers", ["summary"], name: "index_offers_on_summary", using: :btree
 
   create_table "orders", force: true do |t|
     t.string   "reference"
     t.datetime "purchase_date"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "custom_quote_reference"
     t.text     "description"
     t.integer  "supplier_id"
@@ -72,8 +72,8 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.float    "quantity"
     t.text     "description"
     t.string   "status"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "client_id"
     t.integer  "supplier_id"
     t.integer  "order_id"
@@ -90,8 +90,8 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.integer  "supplier_id"
     t.text     "quoted_specifications"
     t.text     "remarks"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.float    "quantity"
     t.string   "unit"
     t.integer  "client_purchased_count",     default: 0
@@ -105,8 +105,8 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.datetime "manufactured_at"
     t.datetime "estimated_delivered_at"
     t.datetime "delivered_at"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "actual_specs"
   end
 
@@ -126,13 +126,13 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.string   "signatory_position"
   end
 
-  add_index "supplier_purchases", ["order_id"], name: "index_supplier_purchases_on_order_id"
-  add_index "supplier_purchases", ["reference"], name: "index_supplier_purchases_on_reference"
+  add_index "supplier_purchases", ["order_id"], name: "index_supplier_purchases_on_order_id", using: :btree
+  add_index "supplier_purchases", ["reference"], name: "index_supplier_purchases_on_reference", using: :btree
 
   create_table "suppliers", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "emails"
     t.string   "contact_numbers"
     t.text     "address"
@@ -149,11 +149,11 @@ ActiveRecord::Schema.define(version: 20130607093706) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
