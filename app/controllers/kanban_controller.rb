ac269a3884495d95400d7f3a46f91dfc1c289f61
Buddy Magsipoc <keikun17@gmail.com>
2013-07-05 15:22:20 +0800

    Not needed.

--- app/controllers/kanban_controller.rb
+++ app/controllers/kanban_controller.rb
@@ -1,7 +1,6 @@
-class KanbanController < ApplicationController
+class KanbanController < ApplicationController	
+	
   def show
-    # @need_quote = Quote.with_pending_requests
-    # @waiting_client_order = Quote.pending_client_order
     @need_supplier_order = Quote.pending_supplier_order
     @for_scheduling = Quote.for_scheduling
     @manufacturing = Quote.manufacturing
