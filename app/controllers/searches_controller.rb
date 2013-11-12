3e56c8657e9c597a14198efd23c56c73399315bb
Buddy Magsipoc <keikun17@gmail.com>
2013-11-12 20:33:58 +0800

    Handle blank searches。

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,17 @@
 class SearchesController < ApplicationController
 
   def search
-    case params[:search][:type]
-    when 'quote'
-      @results = Quote.search(params[:search][:string])
-    when 'order'
-      @results = Order.search(params[:search][:string])
-    else
+    unless params[:search][:string].blank?
+      case params[:search][:type]
+      when 'quote'
+        @results = Quote.search(params[:search][:string])
+      when 'order'
+        @results = Order.search(params[:search][:string])
+      else
 
+      end
+    else
+      @results = []
     end
 
     @results = SearchResult.decorate_collection(@results)
