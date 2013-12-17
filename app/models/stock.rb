b818d914c7b08a3158988091d6e07b84c4036e61
Buddy Magsipoc <keikun17@gmail.com>
2013-12-17 13:51:01 +0800

    Annotate。

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -15,16 +15,17 @@ end
 #
 # Table name: stocks
 #
-#  id            :integer          not null, primary key
-#  quantity      :float
-#  reference     :string(255)
-#  unit          :string(255)
-#  supplier_id   :integer
-#  description   :text
-#  remarks       :text
-#  date_acquired :datetime
-#  date_used_up  :datetime
-#  created_at    :datetime
-#  updated_at    :datetime
+#  id                 :integer          not null, primary key
+#  remaining_quantity :float
+#  reference          :string(255)
+#  unit               :string(255)
+#  supplier_id        :integer
+#  description        :text
+#  remarks            :text
+#  date_acquired      :datetime
+#  date_used_up       :datetime
+#  created_at         :datetime
+#  updated_at         :datetime
+#  initial_quantity   :float
 #
 
