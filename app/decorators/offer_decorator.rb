0c6daf5f9de845a031d244fad81578dc864c731c
Buddy Magsipoc <keikun17@gmail.com>
2014-07-22 14:56:50 +0800

    Delivery Monitoring。

--- app/decorators/offer_decorator.rb
+++ app/decorators/offer_decorator.rb
@@ -156,7 +156,14 @@ class OfferDecorator < ApplicationDecorator
     if date = self.supplier_order_estimated_manufactured_at
       date.to_date.to_s
     else
-      display_none
+      if supplier_order
+        h.content_tag :p do
+          html  = h.link_to_modal("Set Manufacture end date", self.supplier_order, {class: 'btn btn-small btn-success', modal_id: 'exworks'})
+          html << (h.render 'supplier_orders/modals/set_manufacture_date', modal_id: h.dom_id(supplier_order) + 'exworks', supplier_order: supplier_order.decorate)
+        end
+      else
+        display_none
+      end
     end
   end
 
@@ -164,7 +171,14 @@ class OfferDecorator < ApplicationDecorator
     if date = self.supplier_order_estimated_delivered_at
       date.to_date.to_s
     else
-      display_none
+      if supplier_order
+        h.content_tag :p do
+          html  = h.link_to_modal("Set Estimated Delivery", self.supplier_order, {class: 'btn btn-small btn-success', modal_id: 'eta'})
+          html << (h.render 'supplier_orders/modals/set_estimated_delivery_date', modal_id: h.dom_id(supplier_order) + 'eta', supplier_order: supplier_order.decorate)
+        end
+      else
+        display_none
+      end
     end
   end
 
