e189f3e9142aca9235c74b403747b5145a8d1510
Buddy Magsipoc <keikun17@gmail.com>
2014-10-21 14:01:28 +0800

    Product fields for Product form.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :product_fields_attributes
+  has_many :product_fields
+
+  accepts_nested_attributes_for :product_fields, :reject_if => lambda { |r| r[:name].blank? &&
+                           r[:unit].blank? &&
+                           r[:field_type].blank?  }
 end
 
 # == Schema Information
