b5a83d36801f22aa9e0940bbc867df6b2943420a
Buddy Magsipoc <keikun17@gmail.com>
2014-10-27 11:05:57 +0800

    Spacing fix.

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -2,12 +2,12 @@ module QuotesHelper
   def supplier_links(quote)
     suppliers = quote.suppliers.uniq.to_a
     suppliers.collect do |supplier|
-      link_to supplier.name, supplier_path(supplier) 
+      link_to supplier.name, supplier_path(supplier)
     end.join(', ').html_safe
   end
 
   def quote_row_class(quote)
-    case quote.display_status
+    status = case quote.display_status
     when "Awarded"
       'success'
     when "Not Awarded"
@@ -16,6 +16,11 @@ module QuotesHelper
       'error'
     when "No Quote"
       'info'
+    else
+      ''
     end
+
+    row_class = 'multi-content' +  status
+
   end
 end
