50c22bd5312775365dfed58950e03f8287332a49
Buddy Magsipoc <keikun17@gmail.com>
2014-10-29 14:49:54 +0800

    Sort price movement offers by quote date desc.

--- app/controllers/price_movement_controller.rb
+++ app/controllers/price_movement_controller.rb
@@ -1,15 +1,15 @@
 class PriceMovementController < ApplicationController
 
   def index
-    @offer_with_item_codes = Offer.where.not(vendor_item_code: nil).decorate
+    @offer_with_item_codes = Offer.where.not(vendor_item_code: nil).by_quote_date.decorate
   end
 
   def show
-    @offers = Offer.where(vendor_item_code: params[:item_code]).decorate
+    @offers = Offer.where(vendor_item_code: params[:item_code]).by_quote_date.decorate
   end
 
   def uncategorized
-    @offers = Offer.where(vendor_item_code: nil).decorate
+    @offers = Offer.where(vendor_item_code: nil).by_quote_date.decorate
   end
 
 end
