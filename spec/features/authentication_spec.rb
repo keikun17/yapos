b4c424210bfb7f156daa277a4fd603206c610b8b
Buddy Magsipoc <keikun17@gmail.com>
2014-11-01 11:40:08 +0800

    Duplicating this test just to verify if js:true db cleaning strategy works well on CI.

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -2,16 +2,28 @@ require "rails_helper"
 
 feature "Authentication" do
 
-  before do
-    create(:user, email: 'test@email.com', password: 'testpass123', password_confirmation: 'testpass123')
-  end
+  let!(:user) { create(:user, email: 'test@email.com', password: 'testpass123') }
 
   scenario "Signing in" do
+    expect(User.count).to eq(1)
     visit root_path
     expect(page).to have_text("Sign in")
 
-    fill_in "Email", with: "test@email.com"
-    fill_in "Password", with: "testpass123"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Sign in"
+
+    expect(page).not_to have_text("Sign in")
+  end
+
+  scenario "Signing in (js: true)", js: true do
+    expect(User.count).to eq(1)
+    visit root_path
+    expect(page).to have_text("Sign in")
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
 
     click_button "Sign in"
 
