35f8bdcdabfa8880c2a9f09f8e96c62ae76a02cb
Buddy Magsipoc <keikun17@gmail.com>
2014-11-02 16:09:31 +0800

    Refactor link_to_function.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def link_to_remove_fields(name, f, target = 'this')
     anchor_text = content_tag :i, name,  class: 'icon-trash'
-    f.hidden_field(:_destroy) + link_to_function(raw(anchor_text), "remove_fields(#{target})", class: 'btn btn-danger')
+    f.hidden_field(:_destroy) + link_to(raw(anchor_text),'javascript:;', onclick: "remove_fields(#{target})", class: 'btn btn-danger')
   end
 
   def link_to_add_fields(name, f, association, target = 'this')
@@ -11,7 +11,7 @@ module ApplicationHelper
       render(association.to_s.singularize + "_fields", :f => builder)
     end
     anchor_text = content_tag :i, name, class: 'icon-plus'
-    link_to_function(anchor_text, "add_fields(#{target}, \"#{association}\", \"#{escape_javascript(fields)}\")", class: 'btn btn-success')
+    link_to(anchor_text, 'javascript:;', onclick: "add_fields(#{target}, \"#{association}\", \"#{escape_javascript(fields)}\")", class: 'btn btn-success')
   end
 
   def link_to_badge(text, path, options = {})
