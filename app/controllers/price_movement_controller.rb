24d7cb0654a418e6dda346874546616bff6d7dcd
Buddy Magsipoc <keikun17@gmail.com>
2014-11-19 21:03:16 +0800

    Filter by ID now that legacy vendor items are removed.

--- app/controllers/price_movement_controller.rb
+++ app/controllers/price_movement_controller.rb
@@ -1,7 +1,7 @@
 class PriceMovementController < ApplicationController
 
   def index
-    @offer_with_item_codes = Offer.where("vendor_item_code <> ''").by_quote_date.decorate
+    @offer_with_item_codes = Offer.where.not(vendor_item_id: nil).by_quote_date.decorate
   end
 
   def show
