5322be980c428b3f66e8312eefdd9f370dd5111a
Buddy Magsipoc <keikun17@gmail.com>
2014-12-06 23:15:14 +0800

    Search 'exact' spec.

--- spec/features/vendor_item_search_spec.rb
+++ spec/features/vendor_item_search_spec.rb
@@ -24,4 +24,22 @@ feature "Search vendor items containing given property" do
     expect(page).not_to have_content('Abrasive Resistant Conveyor Belt - 200,6,2')
   end
 
+  it "returns all vendor items matching the given spec", js: true do
+    visit search_vendor_item_path
+
+    select "Exact", from: 'Filter'
+    select "Abrasive Resistant Conveyor Belt", from: 'Product'
+
+    fill_in "EP", with: '100'
+    fill_in "Top Cover", with: '5'
+    fill_in "Bottom Cover", with: '3'
+    click_button "Search Items"
+
+    expect(page).to have_content('Abrasive Resistant Conveyor Belt - 100,5,3')
+    expect(page).not_to have_content('Abrasive Resistant Conveyor Belt - 1000,100,5,3')
+
+    expect(page).not_to have_content('Abrasive Resistant Conveyor Belt - 100,5,2')
+    expect(page).not_to have_content('Abrasive Resistant Conveyor Belt - 200,6,2')
+  end
+
 end
