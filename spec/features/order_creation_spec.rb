1730f3bffa23057413a2a1a239ea0703a3c8e85e
Buddy Magsipoc <keikun17@gmail.com>
2014-12-13 18:33:03 +0800

    [Spec] Changing order reference creates a new order record.

--- spec/features/order_creation_spec.rb
+++ spec/features/order_creation_spec.rb
@@ -45,4 +45,42 @@ feature "Order Creation" do
     expect(Order.where(reference: 'PO#1')).not_to be_nil
     expect(Order.where(reference: 'PO#2')).not_to be_nil
   end
+
+  scenario "Changing the reference to an order creates a new order record", js: true do
+    visit root_path
+    click_link "PR#0001"
+    click_link "Edit", match: :first
+
+
+    # Offer #1 for Request #1
+    within(page.all(".offer-line")[0]) do
+      fill_in "Client PO#", with: "PO#1"
+    end
+
+    within(page.all(".offer-line")[2]) do
+      fill_in "Client PO#", with: "PO#2"
+    end
+
+    click_button "Update Quote"
+
+    expect(Order.count).to eq(2)
+    expect(Order.where(reference: 'PO#1')).not_to be_nil
+    expect(Order.where(reference: 'PO#2')).not_to be_nil
+
+    visit root_path
+    click_link "PR#0001"
+    click_link "Edit", match: :first
+
+    # Offer #1 for Request #1
+    within(page.all(".offer-line")[0]) do
+      fill_in "Client PO#", with: "PO#1-revised"
+    end
+
+    click_button "Update Quote"
+
+    expect(Order.count).to eq(3)
+    expect(Order.where(reference: 'PO#1-revised')).not_to be_nil
+    expect(Order.where(reference: 'PO#2')).not_to be_nil
+  end
+
 end
