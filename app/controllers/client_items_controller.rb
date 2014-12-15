9c34d676fefea3c08c13feb4c9d771fb73da877d
Buddy Magsipoc <keikun17@gmail.com>
2014-12-15 21:57:46 +0800

    15 per client item codes page。

--- app/controllers/client_items_controller.rb
+++ app/controllers/client_items_controller.rb
@@ -1,7 +1,7 @@
 class ClientItemsController < ApplicationController
 
   def index
-    @client_item_codes = Request.select(:item_code).where.not(item_code: nil).where.not(item_code: '').uniq.paginate(page: params[:per_page])
+    @client_item_codes_arel = Request.select(:item_code).where.not(item_code: nil).where.not(item_code: '').uniq.paginate(per_page: 15, page: params[:page])
     # @client_item_codes = Request.uniq.pluck(:item_code).paginate(page: params[:per_page])
     @client_item_codes =  @client_item_codes.collect(&:item_code).uniq
   end
