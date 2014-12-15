0bc4c36d5965862b7b01024f52fde24573288e82
Buddy Magsipoc <keikun17@gmail.com>
2014-12-15 21:38:11 +0800

    [wip] Item codes and their Request quantity。

--- app/controllers/client_items_controller.rb
+++ app/controllers/client_items_controller.rb
@@ -1,6 +1,8 @@
 class ClientItemsController < ApplicationController
 
   def index
-    @requests = Request.where.not(item_code: nil).uniq.paginate(page: params[:per_page])
+    @client_item_codes = Request.select(:item_code).where.not(item_code: nil).where.not(item_code: '').uniq.paginate(page: params[:per_page])
+    # @client_item_codes = Request.uniq.pluck(:item_code).paginate(page: params[:per_page])
+    @client_item_codes =  @client_item_codes.collect(&:item_code).uniq
   end
 end
