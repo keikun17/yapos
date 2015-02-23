9510704687b7e25ed9f67f333bf17accba970969
Buddy Magsipoc <keikun17@gmail.com>
2015-02-23 20:07:06 +0800

    Test appearance of orders in the orders page。

--- spec/features/order_creation_spec.rb
+++ spec/features/order_creation_spec.rb
@@ -22,6 +22,9 @@ feature "Order Creation" do
 
     expect(Order.count).to eq(1)
     expect(Order.where(reference: 'PO#1')).not_to be_nil
+
+    click_link "Client Orders"
+    expect(page).to have_link("PO#1")
   end
 
   scenario "Different PO for [Quote1 Offer1] and [Quote# Offer1]", js: true do
@@ -44,6 +47,10 @@ feature "Order Creation" do
     expect(Order.count).to eq(2)
     expect(Order.where(reference: 'PO#1')).not_to be_nil
     expect(Order.where(reference: 'PO#2')).not_to be_nil
+
+    click_link "Client Orders"
+    expect(page).to have_link("PO#1")
+    expect(page).to have_link("PO#2")
   end
 
   scenario "Changing the reference to an order creates a new order record", js: true do
@@ -54,7 +61,7 @@ feature "Order Creation" do
 
     # Offer #1 for Request #1
     within(page.all(".offer-line")[0]) do
-      fill_in "Client PO#", with: "PO#1"
+      fill_in "Client PO#", with: "PO#1-orig"
     end
 
     within(page.all(".offer-line")[2]) do
@@ -64,7 +71,7 @@ feature "Order Creation" do
     click_button "Update Quote"
 
     expect(Order.count).to eq(2)
-    expect(Order.where(reference: 'PO#1')).not_to be_nil
+    expect(Order.where(reference: 'PO#1-orig')).not_to be_nil
     expect(Order.where(reference: 'PO#2')).not_to be_nil
 
     visit root_path
@@ -81,6 +88,10 @@ feature "Order Creation" do
     expect(Order.count).to eq(3)
     expect(Order.where(reference: 'PO#1-revised')).not_to be_nil
     expect(Order.where(reference: 'PO#2')).not_to be_nil
+
+    expect(page).not_to have_link("PO#1-orig")
+    expect(page).to have_link("PO#1-revised")
+    expect(page).to have_link("PO#2")
   end
 
 end
