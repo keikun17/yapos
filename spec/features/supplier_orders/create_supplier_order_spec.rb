8fbb5c0408ecddb3227e22cf21c32e817c50476f
Buddy Magsipoc <keikun17@gmail.com>
2015-03-03 17:24:07 +0800

    Refactor Supplier Order specs。

--- spec/features/supplier_orders/create_supplier_order_spec.rb
+++ spec/features/supplier_orders/create_supplier_order_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 feature "Supplier Order Creation" do
 
-  context "Same RFQ", js: true do
+
+  feature "Search" do
+    it "should be searchable"
+  end
+
+  context "Order within Same RFQ", js: true do
     include_context "Quote with 2 request and 3 offers"
 
-    scenario "[Quote 1 Offer 1] and [Quote 2 Offer 2] ordered" do
+    scenario "Same Supplier PO number" do
       visit root_path
       click_link "PR#0001"
       click_link "Edit", match: :first
@@ -52,13 +57,9 @@ feature "Supplier Order Creation" do
       click_link "Client Orders"
       click_link "Supplier PO required"
       expect(page).to_not have_link("PO#1")
-
-      # Check Printable view
       click_link "Supplier Orders"
 
       expect(page).to have_link("SUPPLIER PO#1")
-
-
       handle_window = window_opened_by { click_link "SUPPLIER PO#1" }
 
       within_window(handle_window) do
@@ -81,12 +82,10 @@ feature "Supplier Order Creation" do
         # Total Price
         expect(page).to have_text("US$869,000.00")
       end
-
-
     end
 
 
-    scenario "[Quote 1 Offer 1] and [Quote 2 Offer 2] in one PO and [Quote 2 Offer 2] in another" do
+    scenario "Different Supplier PO#" do
       visit root_path
       click_link "PR#0001"
       click_link "Edit", match: :first
@@ -108,7 +107,6 @@ feature "Supplier Order Creation" do
       expect(Order.where(reference: 'PO#1')).not_to be_nil
 
       click_link "Client Orders"
-
       expect(page).to have_link("PO#1")
 
       click_link "Supplier PO required"
@@ -306,90 +304,7 @@ feature "Supplier Order Creation" do
 
     end
 
-
   end
-
-
 
 end
 
-feature "Updating Supplier", js: true do
-  include_context "Quote with 1 request and 1 offer"
-  scenario "Supplier PO changes should reflect on the printable view" do
-    visit root_path
-
-    ###########################
-    #  SETTING CLIENT ORDERS
-    ###########################
-
-    # RFQ 1
-    click_link "Price Quotes"
-    click_link "PR# Q1-O1"
-    click_link "Edit", match: :first
-
-    # Offer #1 for Request #1
-    within(page.all(".offer-line")[0]) do
-      fill_in "Client PO#", with: "PO#1"
-    end
-
-    click_button "Update Quote"
-
-    ###########################
-    #  SETTING SUPPLIER ORDERS
-    ###########################
-
-    # Second Client PO for Q1-O1
-    click_link "Client Orders"
-    click_link "Supplier PO required"
-    click_link "PO#1"
-    click_link "Edit", match: :first
-
-    within(page.all(".offer-line")[0]) do
-      find(:css, "textarea[name^='order[offers_attributes]'][name$='[actual_specs]']").set("Personal Chainsaw")
-      find(:css, "input[name^='order[offers_attributes]'][name$='[reference]']").set("SUPPLIER PO#1-orig")
-    end
-
-    click_button "Update Order"
-
-    ###########################
-    # DO EDIT
-    ###########################
-    click_link "Client Orders"
-    click_link "PO#1"
-    click_link "Edit", match: :first
-
-    within(page.all(".offer-line")[0]) do
-      find(:css, "textarea[name^='order[offers_attributes]'][name$='[actual_specs]']").set("Super Personal Chainsaw")
-      find(:css, "input[name^='order[offers_attributes]'][name$='[reference]']").set("SUPPLIER PO#1-revised")
-    end
-
-    click_button "Update Order"
-
-    ###########################
-    # PRINTABLE SUPPLIER ORDER
-    ###########################
-
-    click_link "Supplier Orders"
-    expect(page).not_to have_link("SUPPLIER PO#1-orig")
-    expect(page).to have_link("SUPPLIER PO#1-revised")
-
-
-    handle_window = window_opened_by { click_link "SUPPLIER PO#1-revised", match: :first }
-
-    within_window(handle_window) do
-
-      #header
-      expect(page).to have_text("SUPPLIER PO#1")
-      expect(page).to have_text("Sybil")
-
-      expect(page).to have_text("5.0 piece")
-      expect(page).to have_text("Super Personal Chainsaw")
-      expect(page).to have_text("US$39.00/piece")
-      expect(page).to have_text("US$195.00")
-
-      # Total Price
-      expect(page).to have_text("US$195.00")
-    end
-
-  end
-end
