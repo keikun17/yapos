86dc2e3794549dd44a2f8f2472830c61739953b2
Buddy Magsipoc <keikun17@gmail.com>
2015-03-05 21:46:41 +0800

    Test Quotes search results page。

--- spec/features/quotes/create_quote_spec.rb
+++ spec/features/quotes/create_quote_spec.rb
@@ -10,7 +10,18 @@ feature "Quotes Creation", js: true do
     expect(VendorItem.count).to eq(3)
   end
 
-  it "should be searchable"
+  it "should be searchable" do
+    visit root_path
+
+    search_terms = ['HVB-001', 'Super Seller', 'heavy bolter', 'heavy', 'bolt*']
+
+    search_terms.each do |search_term|
+      fill_in 'search_string', with: search_term
+      select 'quote', from: 'search_type'
+      click_button 'Search'
+      expect(page).to have_link('PR#0001')
+    end
+  end
 
 end
 
