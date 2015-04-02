92b4576083626b69b58bafc4ae0ce57c30db1839
Buddy Magsipoc <keikun17@gmail.com>
2015-04-02 21:57:05 +0800

    Refactor。

--- app/controllers/deliveries_and_invoices_controller.rb
+++ app/controllers/deliveries_and_invoices_controller.rb
@@ -1,16 +1,8 @@
 class DeliveriesAndInvoicesController < ApplicationController
   def index
-    # @offers = Arel::Table.new(:offers)
-    # @offers.where(
-    #   @offers[:delivery_receipt_reference].not_eq('').and(@offers[:delivery_receipt_reference].not_eq(nil))
-    # ).or(@offers[:sales_invoice_reference].not_eq('')).and(@offers[:sales_invoice_reference].not_eq(nil))
-
     @offers = Offer.purchased
-    @offers = @offers.where("offers.delivery_receipt_reference <> '' or offers.sales_invoice_reference <> ''")
-    @offers = @offers.includes([:order, :quote])
-    @offers = @offers.page(params[:page]).per_page(50)
-    # @offers.where{
-    #   ((delivery_receipt_reference.not_eq nil) & (delivery_receipt_reference.not_eq '')) | ((sales_invoice_reference.not_eq nil) & (sales_invoice_reference.not_eq ''))
-    # }.page(params[:page]).per_page(50)
+      .where("offers.delivery_receipt_reference <> '' or offers.sales_invoice_reference <> ''")
+      .includes([:order, :quote])
+      .page(params[:page]).per_page(50)
   end
 end
