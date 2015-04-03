72204befc7dfe0ba4d7336c7a5e8269adf9810fc
Buddy Magsipoc <keikun17@gmail.com>
2015-04-03 10:37:49 +0800

    Remove unused。

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,17 +1,2 @@
 module OrdersHelper
-  def display_quotes(order)
-    str = order.quotes.map.each do |quote|
-      link_to(quote.quote_reference, quote_path(quote)) 
-    end
-    str << order.custom_quote_reference if order.custom_quote_reference.present?
-    str = str.join(', ')
-    raw str
-  end
-
-  def client_names(order)
-    client_names = order.quotes.map(&:client_name)
-    client_names << order.client_name
-    client_names.compact.join(', ')
-  end
-
 end
