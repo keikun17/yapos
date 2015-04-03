fe425cb055940b78c4bc007fd5166b2bb18f94d4
Buddy Magsipoc <keikun17@gmail.com>
2015-04-03 11:15:38 +0800

    Test Supplier and Client pages。

--- spec/features/quotes/create_quote_spec.rb
+++ spec/features/quotes/create_quote_spec.rb
@@ -3,15 +3,58 @@ require 'rails_helper'
 feature "Quotes Creation", js: true do
   include_context "Quote with 2 request and 3 offers"
 
-  it "updates the records", js: true do
+  it "updates the records" do
     expect(Quote.count).to eq(1)
     expect(Request.count).to eq(2)
     expect(Offer.count).to eq(3)
     expect(VendorItem.count).to eq(3)
   end
 
-  it "should appear on the client's page"
-  it "should appear on the supplier's page"
+  it "should appear on the 'all offers' page, client's quotes and offers page and suppliers' quotes and offers page" do
+    # Clients Quotes and offers
+    visit root_path
+    click_link "Client Records"
+    click_link "Blue Buyers"
+    expect(page).to have_link("PR#0001")
+    expect(page).to have_text('heavy bolter')
+    expect(page).to have_text('lighth chainsaw')
+    click_link "Offers"
+    expect(page).to have_link("PR#0001")
+    expect(page).to have_text("2014 Heavy Bolter")
+    expect(page).to have_text("Heavy Bolter 2014 model S1")
+    expect(page).to have_text("light chainsaw")
+    expect(page).to have_text("Billy light chainsaw")
+    expect(page).to have_text("light chainsaw")
+    expect(page).to have_text("ACME Light chainsaw Variant 9001")
+
+    # Supplier Quotes and offers
+    visit root_path
+    click_link "Supplier Records"
+    click_link "Super Seller"
+    click_link "Awaiting Client Purchase (Sorted by Quote Date)"
+    expect(page).to have_link("PR#0001")
+    expect(page).to have_text("2014 Heavy Bolter")
+    expect(page).to have_text("Heavy Bolter 2014 model S1")
+    expect(page).to have_text("light chainsaw")
+    expect(page).to have_text("Billy light chainsaw")
+    expect(page).not_to have_text("ACME Light chainsaw Variant 9001")
+
+    click_link "Client Purchased (Sorted by Order Date)"
+    expect(page).not_to have_link("PR#0001")
+    expect(page).not_to have_text("2014 Heavy Bolter")
+    expect(page).not_to have_text("Heavy Bolter 2014 model S1")
+    expect(page).not_to have_text("light chainsaw")
+    expect(page).not_to have_text("Billy light chainsaw")
+    expect(page).not_to have_text("ACME Light chainsaw Variant 9001")
+
+    click_link "All (Sorted by Quote Date)"
+    expect(page).to have_link("PR#0001")
+    expect(page).to have_text("2014 Heavy Bolter")
+    expect(page).to have_text("Heavy Bolter 2014 model S1")
+    expect(page).to have_text("light chainsaw")
+    expect(page).to have_text("Billy light chainsaw")
+    expect(page).not_to have_text("ACME Light chainsaw Variant 9001")
+  end
 
   it "should be searchable" do
     visit root_path
