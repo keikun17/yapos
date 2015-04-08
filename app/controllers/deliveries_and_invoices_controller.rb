4bafde583ad1b3580bbafd9f49bb5a05dc41b1e7
Buddy Magsipoc <keikun17@gmail.com>
2015-04-08 12:21:13 +0800

    DR sorting。

--- app/controllers/deliveries_and_invoices_controller.rb
+++ app/controllers/deliveries_and_invoices_controller.rb
@@ -3,6 +3,7 @@ class DeliveriesAndInvoicesController < ApplicationController
     @offers = Offer.purchased
       .where("offers.delivery_receipt_reference <> '' or offers.sales_invoice_reference <> ''")
       .includes([:order, :quote])
+      .order(delivery_receipt_reference: :desc)
       .page(params[:page]).per_page(50)
   end
 end
