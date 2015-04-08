23f04be44298140376122a2608e8a3ae791fc574
Buddy Magsipoc <keikun17@gmail.com>
2015-04-08 11:32:18 +0800

    [#69] Mass set delivery dates as well。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,6 +30,11 @@ class OrdersController < ApplicationController
       @order.offers.supplies.update_all({sales_invoice_reference: params[:order][:si_reference]})
     end
 
+    if params[:order][:delivery_date].present?
+      date = Date.new params[:order][:delivery_date]["(1i)"].to_i, params[:order][:delivery_date]["(2i)"].to_i, params[:order][:delivery_date]["(3i)"].to_i
+      @order.supplier_orders.update_all({delivered_at: date})
+    end
+
     redirect_to @order, notice: "Order offers succesfully updated."
 
   end
