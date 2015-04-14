c57a8a5d77ec3445802880dd69efecd05653acdf
Buddy Magsipoc <keikun17@gmail.com>
2015-04-14 13:02:16 +0800

    Order by delivery date。

--- app/controllers/deliveries_and_invoices_controller.rb
+++ app/controllers/deliveries_and_invoices_controller.rb
@@ -2,8 +2,8 @@ class DeliveriesAndInvoicesController < ApplicationController
   def index
     @offers = Offer.purchased
       .where("offers.delivery_receipt_reference <> '' or offers.sales_invoice_reference <> ''")
-      .includes([:order, :quote])
-      .order(delivery_receipt_reference: :desc)
+      .includes([:order, :quote, :supplier_order])
+      .order("supplier_orders.delivered_at desc")
       .page(params[:page]).per_page(50)
   end
 end
