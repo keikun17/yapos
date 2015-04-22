e9d635bc072ba107a4daa0d14059e1fbce38a02c
Buddy Magsipoc <keikun17@gmail.com>
2015-04-23 00:00:14 +0800

    whitelist comment attrs。

--- app/controllers/quote_comments_controller.rb
+++ app/controllers/quote_comments_controller.rb
@@ -1,10 +1,10 @@
 class QuoteCommentsController < ApplicationController
 
   def create
-    @quote = params[:quote_comment][:commentable_id]
-    @commment = Comment.build_from(@quote, @current_user, params[:quote_comment][:body])
-    # @comment.save
-    redirect_to @quote
+    @quote = Quote.find params[:quote_comment][:commentable_id]
+    @comment = Comment.build_from(@quote, current_user.id, params[:quote_comment][:body])
+    @comment.save
 
+    redirect_to @quote, success: 'Comment added succesfully'
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ActiveRecord::Base
   validates :body, :presence => true
   validates :user, :presence => true
 
+  attr_accessible :commentable, :body, :user_id
+
   # NOTE: install the acts_as_votable plugin if you
   # want user to vote on the quality of comments.
   #acts_as_votable
