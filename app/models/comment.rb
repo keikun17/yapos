4729700d4b83728f4b220e47095fbb76c62dd3af
Buddy Magsipoc <keikun17@gmail.com>
2015-04-23 00:59:37 +0800

    Annotate。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -48,3 +48,21 @@ class Comment < ActiveRecord::Base
     commentable_str.constantize.find(commentable_id)
   end
 end
+
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  commentable_id   :integer
+#  commentable_type :string(255)
+#  title            :string(255)
+#  body             :text(65535)
+#  subject          :string(255)
+#  user_id          :integer          not null
+#  parent_id        :integer
+#  lft              :integer
+#  rgt              :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
