64119314b6ad803739ac0ddabc542df91f67a8b3
Buddy Magsipoc <keikun17@gmail.com>
2015-04-29 23:55:55 +0800

    Show PO and DR references。

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,6 +5,8 @@ class Invoice < ActiveRecord::Base
 
   has_many :offers_invoices, dependent: :destroy
   has_many :offers, through: :offers_invoices
+  has_many :orders, through: :offers
 
   has_and_belongs_to_many :payments, join_table: :payments_invoices
+
 end
