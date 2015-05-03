56ce9c12c755bd3765ecb2eff90be9fbe06be206
Buddy Magsipoc <keikun17@gmail.com>
2015-05-03 18:58:58 +0800

    Show RFQ reference on invoices page。

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,6 +6,7 @@ class Invoice < ActiveRecord::Base
   has_many :offers_invoices, dependent: :destroy
   has_many :offers, through: :offers_invoices
   has_many :orders, through: :offers
+  has_many :quotes, through: :offers
 
   has_and_belongs_to_many :payments, join_table: :payments_invoices
 
