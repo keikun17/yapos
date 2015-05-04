f48311da805648af27c0c29a0e8967731f409abc
Buddy Magsipoc <keikun17@gmail.com>
2015-05-04 11:59:00 +0800

    Invoice display page。

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,4 +2,8 @@ class InvoicesController < ApplicationController
   def index
     @invoices = Invoice.all.page(params[:page]).per_page(50)
   end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
 end
