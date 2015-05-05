4a46fc6143fcc0b72228a20d377d544422082102
Buddy Magsipoc <keikun17@gmail.com>
2015-05-05 23:33:54 +0800

    Prevent dupes。

--- app/models/offers_invoice.rb
+++ app/models/offers_invoice.rb
@@ -3,6 +3,7 @@ class OffersInvoice < ActiveRecord::Base
   belongs_to :invoice
 
   validates_uniqueness_of :offer_id, scope: :invoice_id
+  validates_uniqueness_of :invoice_id, scope: :offer_id
 end
 
 # == Schema Information
