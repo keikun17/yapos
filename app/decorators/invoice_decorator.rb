c7a8e25be8e56ee2bd42df723735edc9ea7b6958
Buddy Magsipoc <keikun17@gmail.com>
2015-05-07 12:07:25 +0800

    Show invoice date。

--- app/decorators/invoice_decorator.rb
+++ app/decorators/invoice_decorator.rb
@@ -3,5 +3,9 @@ class InvoiceDecorator < ApplicationDecorator
   def display_amount
     h.number_to_currency(self.amount || 0, unit: Currency::LOCAL_CURRENCY) if self.amount
   end
+
+  def display_date
+    invoice_date.to_date.to_s(:long) if invoice_date
+  end
 end
 
