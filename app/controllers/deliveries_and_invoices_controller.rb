51556a0e015d799331ddc5ac1e73d4ab72c39793
Buddy Magsipoc <keikun17@gmail.com>
2015-05-15 22:25:47 +0800

    Filter Delivery records by client and supplier。

--- app/controllers/deliveries_and_invoices_controller.rb
+++ app/controllers/deliveries_and_invoices_controller.rb
@@ -4,6 +4,16 @@ class DeliveriesAndInvoicesController < ApplicationController
       .where("offers.delivery_receipt_reference <> '' or offers.sales_invoice_reference <> ''")
       .includes([:order, :quote, :supplier_order])
       .order("supplier_orders.delivered_at desc")
-      .page(params[:page]).per_page(50)
+
+    if !params[:client_id].blank?
+      @client = Client.find(params[:client_id])
+      @offers = @offers.where(quotes: {client_id: params[:client_id]})
+    end
+
+    if !params[:supplier_id].blank?
+      @offers = @offers.includes(:supplier).where(suppliers: {id: params[:supplier_id]})
+    end
+
+    @offers = @offers.page(params[:page]).per_page(50)
   end
 end
