24a79fc2b99d1768c567ceb956501216f77d7e6b
Buddy Magsipoc <keikun17@gmail.com>
2015-05-15 22:44:04 +0800

    Refactor。

--- app/decorators/client_decorator.rb
+++ app/decorators/client_decorator.rb
@@ -2,4 +2,12 @@ class ClientDecorator < ApplicationDecorator
 
   delegate_all
 
+  def display_name
+    if client.abbrev.blank?
+      client.name
+    else
+      client.abbrev
+    end
+  end
+
 end
--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -2,12 +2,7 @@ module ClientsHelper
 
   def link_to_client(client)
     if client
-      text = if client.abbrev.blank?
-        client.name
-      else
-        client.abbrev
-      end
-      h link_to(text, client_path(client))
+      h link_to(client.decorate.display_name, client_path(client))
     end
   end
 
