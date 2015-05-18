15cc7e29a8ec00857db4258ba8c9d4458620e9d6
Buddy Magsipoc <keikun17@gmail.com>
2015-05-18 13:01:24 +0800

    Sortable by DR。

--- app/controllers/deliveries_and_invoices_controller.rb
+++ app/controllers/deliveries_and_invoices_controller.rb
@@ -3,7 +3,6 @@ class DeliveriesAndInvoicesController < ApplicationController
     @offers = Offer.purchased
       .where("offers.delivery_receipt_reference <> ''")
       .includes([:order, :quote, :supplier_order])
-      .order("supplier_orders.delivered_at desc")
 
     if !params[:client_id].blank?
       @offers = @offers.where(quotes: {client_id: params[:client_id]})
@@ -13,6 +12,31 @@ class DeliveriesAndInvoicesController < ApplicationController
       @offers = @offers.includes(:supplier).where(suppliers: {id: params[:supplier_id]})
     end
 
+    if params[:sort].present?
+      @offers = sort(@offers, params[:sort], params[:direction])
+    end
+
     @offers = @offers.page(params[:page]).per_page(50)
   end
+
+
+  private
+
+  def sort(offers, by, direction)
+
+    direction = case direction
+                when 'desc'
+                  :desc
+                when 'asc'
+                  :asc
+                end
+
+    case by
+    when "dr"
+      offers = offers.order(delivery_receipt_reference: direction)
+    end
+
+    offers
+  end
+
 end
