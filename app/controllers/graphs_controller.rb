869027ae3e258757f50870b4e00e454e5302e212
Buddy Magsipoc <keikun17@gmail.com>
2015-10-14 15:40:34 +0800

    Refactor。

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -8,27 +8,56 @@ class GraphsController < ApplicationController
   end
 
   def performance
+    # QUOTES
     @quotes_count = Quote.where(created_at: [1.year.ago..Time.zone.now]).group_by_month(:created_at, time_zone: Time.zone).count
+    @quotes_one_year_ago = Quote.where(created_at: [2.year.ago..1.year.ago]).group_by_month(:created_at, time_zone: Time.zone).count
+    @quotes_two_years_ago = Quote.where(created_at: [3.year.ago..2.year.ago]).group_by_month(:created_at, time_zone: Time.zone).count
 
-    @prev_quotes_count = Quote.where(created_at: [2.year.ago..1.year.ago]).group_by_month(:created_at, time_zone: Time.zone).count
+    @quotes_one_year_ago = align_chartkick_dates(@quotes_one_year_ago, 1)
+    @quotes_two_years_ago = align_chartkick_dates(@quotes_two_years_ago, 2)
 
-    @x = @prev_quotes_coun
-    @prev_quotes_count.keys.each do |date|
-      new_date = Time.zone.at( date + 1.year).beginning_of_month
+    @quote_dates = [
+      {name: Time.now.year, data: @quotes_count},
+      {name: 1.year.ago.year, data: @quotes_one_year_ago},
+      {name: 2.years.ago.year, data: @quotes_two_years_ago}
+    ]
 
-      # Rename the key
-      @prev_quotes_count[new_date] = @prev_quotes_count.delete(date)
-    end
 
+    # ORDERS
     @orders_count = Order.where(created_at: [1.year.ago..Time.zone.now]).group_by_month(:created_at, time_zone: Time.zone).count
-    @prev_orders_count = Order.where(created_at: [2.year.ago..1.year.ago]).group_by_month(:created_at, time_zone: Time.zone).count
+    @orders_one_year_ago = Order.where(created_at: [2.year.ago..1.year.ago]).group_by_month(:created_at, time_zone: Time.zone).count
+    @orders_two_years_ago = Order.where(created_at: [3.year.ago..2.year.ago]).group_by_month(:created_at, time_zone: Time.zone).count
 
-    @prev_orders_count.keys.each do |date|
-      new_date = Time.zone.at( date + 1.year).beginning_of_month
+    @orders_one_year_ago = align_chartkick_dates(@orders_one_year_ago, 1)
+    @orders_two_years_ago = align_chartkick_dates(@orders_two_years_ago, 2)
+
+    @order_dates = [
+      {name: Time.now.year, data: @orders_count},
+      {name: 1.year.ago.year, data: @orders_one_year_ago},
+      {name: 2.years.ago.year, data: @orders_two_years_ago},
+    ]
+
+  end
+
+  private
+
+  # Change the year to the current year so chartkick's plotted points will line
+  # up
+  #
+  #  Arguments :
+  #    date_hash  - hash - date hash format use by chartkick (keys are the date,
+  #                        value is the count)
+  #    offset     - int  - from how many years ago is the data in the date hash?
+  def align_chartkick_dates(date_hash, offset)
+
+    date_hash.keys.each do |date|
+      new_date = Time.zone.at( date + offset.years).beginning_of_month
 
       # Rename the key
-      @prev_orders_count[new_date] = @prev_orders_count.delete(date)
+      date_hash[new_date] = date_hash.delete(date)
     end
+
+    date_hash
   end
 
 end
