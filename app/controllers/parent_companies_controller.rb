c0f65269ac081e638b677f9d2b0e641ea606dd55
Buddy Magsipoc <keikun17@gmail.com>
2015-12-17 11:41:52 +0800

    Paginate quotes on parent company pages。

--- app/controllers/parent_companies_controller.rb
+++ app/controllers/parent_companies_controller.rb
@@ -1,12 +1,14 @@
 class ParentCompaniesController < ApplicationController
 
   def index
-    @parent_companies = ParentCompany.all
+    @parent_companies = ParentCompany.page(params[:page]).per_page(40)
     respond_with(@parent_companies)
   end
 
   def show
     @parent_company = ParentCompany.find(params[:id])
+    @quotes = @parent_company.quotes.page(params[:page]).per_page(40)
+
     respond_with(@parent_company)
   end
 
