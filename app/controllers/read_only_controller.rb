7c23cc6ea1c998a1279d63bf6731d07d0ac600cc
Buddy Magsipoc <keikun17@gmail.com>
2016-04-14 14:25:20 +0800

    Reduce record count displayed for read only from 200 to 75。

--- app/controllers/read_only_controller.rb
+++ app/controllers/read_only_controller.rb
@@ -2,6 +2,6 @@ class ReadOnlyController < ApplicationController
 
   #TODO : ADD TESTS FOR THIS VIEW
   def index
-    @orders = Order.with_offers.order(created_at: :desc).limit(200)
+    @orders = Order.with_offers.order(created_at: :desc).limit(70)
   end
 end
