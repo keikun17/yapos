ffe29363e39e55bf3b59fb9928cec4ff53e0ad12
Buddy Magsipoc <keikun17@gmail.com>
2016-04-24 12:58:30 +0800

    N+1。

--- app/controllers/read_only_controller.rb
+++ app/controllers/read_only_controller.rb
@@ -2,6 +2,7 @@ class ReadOnlyController < ApplicationController
 
   #TODO : ADD TESTS FOR THIS VIEW
   def index
-    @orders = Order.with_offers.order(created_at: :desc).limit(70)
+    @orders = Order.includes(:clients, offers: [:request]).
+      with_offers.order(created_at: :desc).limit(70)
   end
 end
