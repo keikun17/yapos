be8c1b7cc1373c811ffa33aca9b7640c7b35cf34
Buddy Magsipoc <keikun17@gmail.com>
2016-05-01 16:29:48 +0800

    Readyonly records : 70 -> 50。

--- app/controllers/read_only_controller.rb
+++ app/controllers/read_only_controller.rb
@@ -3,6 +3,6 @@ class ReadOnlyController < ApplicationController
   #TODO : ADD TESTS FOR THIS VIEW
   def index
     @orders = Order.includes(:clients, offers: [:request, :supplier_purchase, :supplier, :supplier_order ]).
-      with_offers.order(created_at: :desc).limit(70)
+      with_offers.order(created_at: :desc).limit(50)
   end
 end
