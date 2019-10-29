1ceec2dca5c84d86cab5e29dc6db86f32e68e80f
Buddy Magsipoc <keikun17@gmail.com>
2019-10-29 17:54:21 +0800

    update rails_helper to remove unused stuff。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,9 @@
-# Code Climate stuff
-require 'dotenv'
-Dotenv.load
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'database_cleaner'
+# require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
